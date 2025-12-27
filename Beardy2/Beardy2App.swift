1581145730ede517006048115f07ce0e67294dd4
Shkurko-KYu <shkurko83@gmail.com>
2025-12-27 13:52:01 +0300

    Initial Commit

---
+++ Beardy2/Beardy2App.swift
@@ -0,0 +1,17 @@
+//
+//  Beardy2App.swift
+//  Beardy2
+//
+//  Created by Butt Simpson on 27.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct Beardy2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
